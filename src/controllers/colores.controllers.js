7c710b15794370de5553a39c309c9c8acde2ad09
Andres Santamarina <andresesantamarina@gmail.com>
2024-03-15 21:40:54 -0300

    agregue la logica para editar un color

--- src/controllers/colores.controllers.js
+++ src/controllers/colores.controllers.js
@@ -25,3 +25,22 @@ export const crearColor = async (req, res) => {
         })
     }
 }
+
+export const editarColor = async (req, res)=>{
+    try {
+        const buscarColor = await Color.findById(req.params.id)
+        if(!buscarColor){
+            return res.status(404).json({
+                mensaje: "No se pudo editar el color, el id es incorrecto"
+            })
+        }
+        await Color.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({
+            mensaje: "El color fue modificado correctamente"
+        })
+    } catch (error) {
+        res.status(500).json({
+            mensaje: "Ocurrió un error al intentar editar el color"
+        })
+    }
+}
--- src/routes/colores.routes.js
+++ src/routes/colores.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { crearColor, listarColores } from "../controllers/colores.controllers.js";
+import { crearColor, editarColor, listarColores } from "../controllers/colores.controllers.js";
 
 
 const router = Router();
 
 router.route('/colores').get(listarColores).post(crearColor)
+router.route('/color/:id').put(editarColor)
 
 export default router;
