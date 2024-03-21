902dc7f912a72e942273bee057f8e5feb3dd2758
Andres Santamarina <andresesantamarina@gmail.com>
2024-03-20 21:32:48 -0300

    agregue especificamente las opciones de cors

--- index.js
+++ index.js
@@ -9,14 +9,19 @@ import './src/database/database.js'
 
 const app = express();
 app.set('port', process.env.PORT || 4000);
-app.listen(app.get('port'), ()=>{
-    console.log('Estoy en el puerto '+ app.get('port'))
+app.listen(app.get('port'), () => {
+    console.log('Estoy en el puerto ' + app.get('port'))
 })
 
-app.use(cors());
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}));
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
